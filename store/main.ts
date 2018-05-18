dc7252a286f044c3ed051f915d8a4455f4461e52
David Kuo <s50407s@gmail.com>
2018-05-19 00:01:04 +0800

    [Vuex][Main] Experimental caches

--- store/main.ts
+++ store/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import {
   ActionContext,
   ActionTree,
+  GetterTree,
   MutationTree,
 } from 'vuex'
 import {
@@ -10,11 +11,16 @@ import {
 import {
   name as endpointStateName,
 } from './endpoints'
+import {
+  Languages,
+} from './i18n'
+import { fail } from 'assert';
 
 export const name = 'main'
 
 export const types = {
-  UPDATE: 'update'
+  UPDATE: 'update',
+  UPDATE_CACHE: 'update_cache',
 }
 
 type Event = {
@@ -32,7 +38,7 @@ type Contactable = {
 type COSCUPYears = '2006' | '2007' | '2008' | '2009' |
   '2010' | '2011' | '2012' | '2013' | '2014' | '2015' | '2016'
 
-export type State = {
+export type BaseState = {
   description: string
   place: string
   registration: Event
@@ -49,7 +55,15 @@ export type State = {
   }
 }
 
-export const state = (): State => ({} as State)
+export type State = BaseState & {
+  __caches: {
+    [Lang in keyof typeof Languages]?: BaseState
+  }
+}
+
+export const state = (): State => ({
+  __caches: {},
+} as State)
 
 export interface Actions<S, R> extends ActionTree<S, R> {
   nuxtServerInit(context: ActionContext<S, R>): void
@@ -61,18 +75,40 @@ export const actions: Actions<State, RootState> = {
     await dispatch('fetchData')
   },
 
-  async fetchData({ commit, rootState }) {
+  async fetchData({ commit, state: { __caches: caches }, rootState }) {
     const locale = rootState.i18n.locale
-    const endpoint = rootState[endpointStateName][locale].main
-    const response = await fetch(endpoint)
-    const datas = await response.json()
+    let failable = false
+    if (caches[locale]) {
+      commit(types.UPDATE, caches[locale])
+      failable = true
+    }
 
-    commit(types.UPDATE, datas)
+    try {
+      const endpoint = rootState[endpointStateName][locale].main
+      const response = await fetch(endpoint)
+      const datas = await response.json()
+
+      commit(types.UPDATE_CACHE, { locale, datas })
+      commit(types.UPDATE, datas)
+    } catch (e) {
+      if (failable) return;
+
+      throw e
+    }
   }
 }
 
 export const mutations: MutationTree<State> = {
+  [types.UPDATE_CACHE](state, { locale, datas }) {
+    state.__caches = {
+      ...state.__caches,
+
+      [locale]: datas,
+    }
+  },
+
   [types.UPDATE](state, datas) {
-    Object.entries(datas).forEach(([key, value]) => (state[key] = value))
+    Object.entries(datas)
+      .forEach(([key, value]) => (state[key] = value))
   }
 }
