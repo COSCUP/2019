d94011d20ca89885d5c1b7920189f5d7c2e340a7
David Kuo <s50407s@gmail.com>
2018-06-04 19:38:32 +0800

    [Component] Add RatioBox

---
+++ components/RatioBox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="ratio-box" :style="{ '--ratio': cssRatio }">
+    <div class="content" v-bind="$attrs">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue,
+} from 'nuxt-property-decorator'
+
+@Component({
+  inheritAttrs: false,
+  props: {
+    ratio: {
+      type: String,
+      required: true,
+    },
+  },
+})
+export default class extends Vue {
+  get cssRatio() {
+    const ratio: String = this.$props.ratio
+    const [w, h = 1] = ratio.split(':')
+
+    return `${100.0 * Number(h) / Number(w)}%`
+  }
+}
+</script>
+
+<style scoped>
+.ratio-box {
+  width: 100%;
+  position: relative;
+}
+
+.ratio-box:before {
+  content: "";
+  display: block;
+  padding-top: 100%;
+  padding-top: var(--ratio);
+}
+
+.ratio-box > .content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+</style>
