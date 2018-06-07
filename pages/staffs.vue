40f9ba2fee4cf376dff70233e948e37b8e6fbf9c
David Kuo <s50407s@gmail.com>
2018-06-08 00:41:47 +0800

    [Pages][Staffs] Everybody has a head

--- pages/staffs.vue
+++ pages/staffs.vue
@@ -3,8 +3,9 @@
     <Card class="container" v-for="team in teams" :key="team.key">
       <h1>{{ team.title }}</h1>
       <ul>
-        <li v-for="member in team.members" :key="member.name">
-          {{ member.name }}
+        <li v-for="member in team.members" :key="member.name" :title="member.name">
+          <img :src="member.image" />
+          <div class="name">{{ member.name }}</div>
         </li>
       </ul>
     </Card>
@@ -67,6 +68,48 @@ main.staffs ul {
 }
 
 main.staffs li {
-  margin-right: 2.5em;
+  margin: 1em 1em 0 0;
+  position: relative;
+  max-width: 100px;
+  max-height: 100px;
+
+  line-height: 0;
+}
+
+main.staffs li .name {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding-bottom: .8em;
+
+  display: flex;
+  filter: opacity(0);
+  opacity: 0;
+
+  color: #fff;
+  background: rgba(128, 128, 128, .6);
+  background: linear-gradient(to bottom,
+    transparent 0%,
+    rgba(0, 0, 0, 0.6)
+  );
+  font-size: .8em;
+  text-align: center;
+  line-height: 1em;
+  justify-content: center;
+  align-items: end;
+
+  transition: filter .2s cubic-bezier(0.4, 0.0, 0.2, 1),
+    opacity .2s cubic-bezier(0.4, 0.0, 0.2, 1);
+}
+
+main.staffs li:hover .name {
+  filter: opacity(1);
+  opacity: 1;
+}
+
+main.staffs li img {
+  max-width: 100%;
 }
 </style>
