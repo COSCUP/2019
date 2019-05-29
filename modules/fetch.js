93fff03053bdefa40cb8e78b4c5353a266e11905
David Kuo <s50407s@gmail.com>
2019-05-29 22:50:34 +0800

    [Fixup] Change fetch import method for new webpack

--- modules/fetch.js
+++ modules/fetch.js
@@ -5,7 +5,7 @@ module.exports = function() {
   this.extendBuild(({ plugins, ...config }, { isClient }) => {
     if (isClient) {
       plugins.push(new webpack.ProvidePlugin({
-        'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+        'fetch': 'exports-loader?self.fetch!whatwg-fetch'
       }))
     } else {
       plugins.push(new webpack.ProvidePlugin({
