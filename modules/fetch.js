3fcf3fa07265d00aaec99b7e3dc1d058cde74289
Erickson Zhuang <bingluen0224@gmail.com>
2019-05-30 00:55:26 +0800

    Fix error load whatwg-fetch on webpack

--- modules/fetch.js
+++ modules/fetch.js
@@ -5,7 +5,7 @@ module.exports = function() {
   this.extendBuild(({ plugins, ...config }, { isClient }) => {
     if (isClient) {
       plugins.push(new webpack.ProvidePlugin({
-        'fetch': 'exports-loader?self.fetch!whatwg-fetch'
+        'fetch': 'exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd',
       }))
     } else {
       plugins.push(new webpack.ProvidePlugin({
