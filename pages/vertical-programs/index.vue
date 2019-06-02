5eb674e28688da83e36e9345ebf11640f87082fa
Erickson Zhuang <bingluen0224@gmail.com>
2019-06-02 11:39:09 +0800

    Refactoring programs table

--- pages/vertical-programs/index.vue
+++ pages/vertical-programs/index.vue
@@ -1,442 +1,406 @@
 <template>
-  <div id="schedule">
-    <div class="days">
-      <ul>
-        <template v-for="(day, index) in eventDates">
-          <li :key="index">{{ `Day ${index}` }} {{ getDateString(day) }}</li>
-        </template>
-      </ul>
-    </div>
-    <div
-      id="timetable"
-      :style="
-        `--table: ${gridTemplateRows}; --list: ${listTemplateRow};--length: ${
-          locations.length
-        }`
-      "
-    >
-      <ul id="locations">
-        <template v-for="(location, index) in locations">
-          <li :key="index">
-            Room
-            <strong>{{ location }}</strong>
-          </li>
-        </template>
-      </ul>
-      <ul id="programs">
-        <template
-          v-for="(timeSlot, index) in programStarts.filter(
-            (v, i, a) => a.indexOf(v) === i
-          )"
-        >
-          <li
-            :key="`time-${index}`"
-            class="time"
-            :style="`grid-row-start: t${getTimeSlug(timeSlot)}`"
-          >
-            {{ getTimeSlug(timeSlot, true) }}
-          </li>
-        </template>
-        <template v-for="(program, index) in programs">
-          <li
-            :key="`program-${index}`"
-            class="program"
-            :style="{
-              '--room': `room${toUnicode(program.track.room)}`,
-              '--start': `t${getTimeSlug(program.start)}`,
-              '--end': `t${getTimeSlug(program.end)}`
-            }"
-          >
-            <article>
-              <footer>
-                <span class="period">
-                  {{
-                    `${getTimeSlug(program.start, true)} ~ ${getTimeSlug(
-                      program.end,
-                      true
-                    )}`
-                  }}
-                </span>
-                <span class="track">{{ program.track.title }}</span>
-              </footer>
-              <header>
-                <h4>{{ program.title }}</h4>
-              </header>
-              <span class="location">{{ program.track.room }}</span>
-              <span class="length">{{ getLength(program) }}</span>
-              <span class="language">{{ program.language }}</span>
-            </article>
-          </li>
-        </template>
-      </ul>
-    </div>
-  </div>
+	<div id="schedule">
+		<nav class="days">
+			<template v-for="(day, index) in eventDay">
+				<a :key="index">{{ `Day ${index}` }} {{ `${day.year}/${day.month}/${day.date}` }}</a>
+			</template>
+		</nav>
+		<div
+			id="timetable"
+			:style="`--table: ${cssGridTemplateRows}; --list: ${cssListTemplateRow}; --length: ${rooms.length}; --areas: '${cssGridAreas}'`"
+		>
+			<ul id="rooms">
+				<template v-for="(room, index) in rooms">
+					<li :key="index">
+						Room
+						<strong>{{ room.name }}</strong>
+					</li>
+				</template>
+			</ul>
+			<ul id="programs">
+				<template v-for="(start, index) in programStartTimeUnique">
+					<li
+						:key="`time-${index}`"
+						class="time"
+						:style="`grid-row-start: t${getTimeSlug(start)}`"
+					>{{ getTimeSlug(start) }}</li>
+				</template>
+
+				<template v-for="(program, index) in programs">
+					<li
+						v-if="program.start && program.end && program.room"
+						:key="`program-${index}`"
+						class="program"
+						:style="{
+            '--room': `${program.room.id}`,
+            '--start': `t${getTimeSlug(program.start)}`,
+            '--end': `t${getTimeSlug(program.end)}`
+          }"
+					>
+						<article>
+							<footer>
+								<span class="period">{{ `${getTimeSlug(program.start)} ~ ${getTimeSlug(program.end)}` }}</span>
+								<span
+									class="track"
+									v-if="program.tags.length && program.tags[1]"
+								>{{ `${program.tags[1].name}` }}</span>
+								<header>
+									<h4>{{ program.title }}</h4>
+								</header>
+								<span class="room">{{ program.room.name }}</span>
+								<span class="length">{{ `${program.period} mins` }}</span>
+								<span
+									class="language"
+									v-if="program.tags.length && program.tags[0]"
+								>{{ `${program.tags[0].name}` }}</span>
+							</footer>
+						</article>
+					</li>
+				</template>
+			</ul>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 // packages
-import { Component, Vue } from 'nuxt-property-decorator'
-import { namespace } from 'vuex-class'
+import { Component, Vue } from "nuxt-property-decorator";
+import { namespace } from "vuex-class";
 
 // stores
-import { name as programsStoreName } from '~/store/programs'
+import { name as programsStoreName, DateTime, Program } from "~/store/programs";
 
-import Program from './_program.vue'
-
-const ProgramsState = namespace(programsStoreName).State
+const ProgramsState = namespace(programsStoreName).State;
 
 @Component({
-  name: 'Programs',
-  components: {
-    Program
-  }
+	name: "Programs"
 })
 class Programs extends Vue {
-  @ProgramsState talks
-  @ProgramsState tracks
-  get gridTemplateRows() {
-    return this.programTimeSlots
-      .filter((v, i, a) => a.indexOf(v) === i)
-      .map(start => {
-        return `[t${this.getTimeSlug(start)}] minmax(1em, auto)`
-      })
-      .join(' ')
-  }
+	@ProgramsState programs;
+	@ProgramsState rooms;
+	@ProgramsState tags;
+	@ProgramsState types;
+	@ProgramsState eventDay;
+	mounted() {
+		this.$store.dispatch("clientsFirstFetch", this.$options.fetch);
+	}
 
-  get listTemplateRow() {
-    return this.programTimeSlots
-      .map((time, i, arr) =>
-        i > 0 && new Date(time).getTime() === new Date(arr[i - 1]).getTime()
-          ? 'auto'
-          : `[t${this.getTimeSlug(time)}] auto auto`
-      )
-      .join(' ')
-  }
+	async fetch({ store: { dispatch } }) {
+		await dispatch(`${programsStoreName}/fetchData`);
+	}
 
-  get eventDates() {
-    return this.talks
-      .map(talk => talk.start)
-      .filter((v, i, a) => {
-        const previous = a.slice(0, i).map(p => new Date(p).getDate())
-        const current = new Date(v).getDate()
-        return previous.indexOf(current) === -1
-      })
-      .sort()
-  }
+	get currentDay(): DateTime {
+		const today = new Date();
 
-  get defaultShowDate() {
-    const today = new Date()
-    return (
-      this.eventDates.find(date => {
-        const eventDate = new Date(date)
-        return (
-          today.getFullYear() === eventDate.getFullYear() &&
-          today.getMonth() === eventDate.getMonth() &&
-          today.getDate() === eventDate.getDate()
-        )
-      }) ||
-      (this.eventDates && this.eventDates[0])
-    )
-  }
+		const day = this.eventDay.find(
+			(current: DateTime) =>
+				current.year === today.getFullYear() &&
+				current.month === today.getMonth() &&
+				current.date === today.getDate()
+		);
 
-  get programStarts() {
-    const todayTalks = this.talks.filter(talk => {
-      const talkDate = new Date(talk.start)
-      const showDate = new Date(this.defaultShowDate)
-      return (
-        talkDate.getFullYear() === showDate.getFullYear() &&
-        talkDate.getMonth() === showDate.getMonth() &&
-        talkDate.getDate() === showDate.getDate()
-      )
-    })
-    return todayTalks.map(talk => talk.start).sort()
-  }
+		return day !== undefined ? day : this.eventDay[0];
+	}
 
-  get programTimeSlots() {
-    const todayTalks = this.talks.filter(talk => {
-      const talkDate = new Date(talk.start)
-      const showDate = new Date(this.defaultShowDate)
-      return (
-        talkDate.getFullYear() === showDate.getFullYear() &&
-        talkDate.getMonth() === showDate.getMonth() &&
-        talkDate.getDate() === showDate.getDate()
-      )
-    })
-    return todayTalks
-      .reduce((timeSlots, talk) => timeSlots.concat(talk.start, talk.end), [])
-      .sort()
-  }
+	get todayPrograms(): Program[] {
+		return this.programs.filter(
+			(program: Program): boolean => {
+				return program.start
+					? program.start.date === this.currentDay.date
+					: false;
+			}
+		);
+	}
 
-  get locations() {
-    return this.talks
-      .filter(talk => {
-        const talkDate = new Date(talk.start)
-        const showDate = new Date(this.defaultShowDate)
-        return (
-          talkDate.getFullYear() === showDate.getFullYear() &&
-          talkDate.getMonth() === showDate.getMonth() &&
-          talkDate.getDate() === showDate.getDate()
-        )
-      })
-      .map(talk => talk.track)
-      .map(track => track.room)
-      .filter((v, i, a) => a.indexOf(v) === i)
-      .sort()
-  }
+	get programTimeSlot(): DateTime[] {
+		return this.todayPrograms.reduce(
+			(carry: Array<DateTime>, cv: Program, index): Array<DateTime> => {
+				carry.push(cv.start, cv.end);
+				return carry;
+			},
+			[]
+		);
+	}
 
-  get programs() {
-    return this.talks
-      .filter(talk => {
-        const talkDate = new Date(talk.start)
-        const showDate = new Date(this.defaultShowDate)
-        return (
-          talkDate.getFullYear() === showDate.getFullYear() &&
-          talkDate.getMonth() === showDate.getMonth() &&
-          talkDate.getDate() === showDate.getDate()
-        )
-      })
-      .sort((a, b) => a.startAt - b.startAt)
-  }
+	get programStartTime(): DateTime[] {
+		return this.programTimeSlot.filter(
+			(current: DateTime, index: number, arr: DateTime[]) => index % 2 === 0
+		);
+	}
 
-  mounted() {
-    this.$store.dispatch('clientsFirstFetch', this.$options.fetch)
-  }
+	get programStartTimeUnique(): DateTime[] {
+		return this.programStartTime.filter(
+			(current: DateTime, index: number, arr: DateTime[]) =>
+				arr.findIndex(exists => exists.timestamp === current.timestamp) ===
+				index
+		);
+	}
 
-  async fetch({ store: { dispatch } }) {
-    await dispatch(`${programsStoreName}/fetchData`)
-  }
+	get cssGridTemplateRows() {
+		return this.programTimeSlot
+			.filter(
+				(v, i, a) =>
+					a.findIndex(exists => exists.timestamp === v.timestamp) === i
+			)
+			.sort((a: DateTime, b: DateTime) => a.timestamp - b.timestamp)
+			.map((time: DateTime) => {
+				return `[t${this.getTimeSlug(time)}] minmax(1em, auto)`;
+			})
+			.join(" ");
+	}
 
-  getTimeSlug(datetime, withColon = false) {
-    const time = datetime instanceof Date ? datetime : new Date(datetime)
-    return `${this.padStartWithZero(time.getHours())}${
-      withColon ? ':' : ''
-    }${this.padStartWithZero(time.getMinutes())}`
-  }
+	get cssGridAreas() {
+		return this.rooms.map(room => room.id).join(" ");
+	}
 
-  padStartWithZero(number) {
-    return number < 10 ? `0${number}` : number.toString()
-  }
+	get cssListTemplateRow() {
+		return this.programStartTime
+			.map((current: DateTime, index: number, arr: DateTime[]) =>
+				index > 0 && current.timestamp === arr[index - 1].timestamp
+					? "auto"
+					: `[t${this.getTimeSlug(current)}] auto auto`
+			)
+			.join(" ");
+	}
 
-  getDateString(date) {
-    const dateObj = new Date(date)
+	getTimeSlug = (dateTime: DateTime): string => {
+		const hours = this.padStartWithZero(dateTime.hour);
+		const minutes = this.padStartWithZero(dateTime.minute);
+		return `${hours}:${minutes}`;
+	};
 
-    return `${dateObj.getFullYear()}/${dateObj.getMonth()}/${dateObj.getDate()}`
-  }
-
-  toUnicode(str) {
-    return str
-      .split('')
-      .map(function(value) {
-        const temp = value
-          .charCodeAt(0)
-          .toString(16)
-          .toUpperCase()
-        if (temp.length > 2) {
-          return '-' + temp
-        }
-        return value
-      })
-      .join('')
-  }
-
-  getLength(program) {
-    const start =
-      program.start instanceof Date ? program.start : new Date(program.start)
-    const end =
-      program.end instanceof Date ? program.end : new Date(program.end)
-
-    return `${(end.getTime() - start.getTime()) / 1000 / 60} mins`
-  }
+	padStartWithZero(number) {
+		return number < 10 ? `0${number}` : number.toString();
+	}
 }
-export default Programs
+export default Programs;
 </script>
 
 <style lang="stylus">
 .days {
-  ul {
-    display: flex;
-    justify-content flex-end;
-    list-style: none;
-  }
-  li {
-    display: block;
-  }
+	a {
+		display: inline-block;
+		text-decoration: none;
+		border: none;
+		line-height: auto;
+		color: inherit;
+	}
+
+  .active {
+        color: #009a79;
+        border-bottom: 5px solid;
+    }
+
+	@media only screen and (max-width: 719px) {
+		position: fixed;
+		top: 0;
+		width: 100%;
+		background: linear-gradient(-315deg, #f9fea5, #20e2d7);
+		display: flex;
+		z-index: 999;
+
+		a {
+			height: 64px;
+			flex: 1 0 auto;
+			text-align: center;
+			line-height: 64px;
+		}
+	}
+
+	@media only screen and (min-width: 720px) {
+		text-align: right;
+
+		a {
+			color: #b8d2cf;
+			padding: 0.5em;
+		}
+	}
+
+	@media only screen and (min-width: 1000px) {
+		margin: 0 -32px 1em;
+		font-size: 3em;
+		position: static;
+
+		a {
+			margin-right: 0.5em;
+		}
+	}
 }
 
 #timetable {
-  position: relative;
+	position: relative;
 }
 
-#locations {
-  display: none;
-  list-style: none;
-  text-align: center;
-  position: sticky;
-  position: -webkit-sticky;
-  top: 65px;
-  contain: layout;
-  background-color: rgba(255, 255, 255, 0.8);
-  background-attachment: fixed;
-  z-index: 9;
-  margin: 0;
+#rooms {
+	display: none;
+	list-style: none;
+	text-align: center;
+	position: sticky;
+	position: -webkit-sticky;
+	top: 65px;
+	contain: layout;
+	background-color: rgba(255, 255, 255, 0.8);
+	background-attachment: fixed;
+	z-index: 9;
+	margin: 0;
 
-  li {
-    flex: 1 0 calc(((100% - 7 * 0.5em) / 8));
-    font-size: smaller;
-    color: rgba(0, 0, 0, 0.4);
+	li {
+		flex: 1 0 calc(((100% - 7 * 0.5em) / 8));
+		font-size: smaller;
+		color: rgba(0, 0, 0, 0.4);
 
-    &:not(:first-child) {
-      margin-left: 0.5em;
-    }
-  }
+		&:not(:first-child) {
+			margin-left: 0.5em;
+		}
+	}
 
-  strong {
-    display: block;
-    font-size: 2em;
-    margin: -0.3em 0 0.5em;
-    color: rgba(0, 0, 0, 0.7);
-  }
+	strong {
+		display: block;
+		font-size: 2em;
+		margin: -0.3em 0 0.5em;
+		color: rgba(0, 0, 0, 0.7);
+	}
 
-  @media only screen and (min-width: 1000px) {
-    display: flex;
-  }
+	@media only screen and (min-width: 1000px) {
+		display: flex;
+	}
 }
 
 .time {
-  font-size: 20px;
-  color: #009a79;
-  background-color: #ecf5f4;
-  padding: 0.5em;
+	font-size: 20px;
+	color: #009a79;
+	background-color: #ecf5f4;
+	padding: 0.5em;
 
-  @supports (position: -webkit-sticky) {
-    position: -webkit-sticky;
-    top: 69px;
-    z-index: 9;
-  }
+	@supports (position: -webkit-sticky) {
+		position: -webkit-sticky;
+		top: 69px;
+		z-index: 9;
+	}
 
-  @media only screen and (min-width: 720px) {
-    position: sticky;
-    margin: 0;
-    padding: 0.5em 64px;
-    border-top: 1px solid #b8d2cf;
-    grid-column: 1 / span 2;
-    top: 64px;
-  }
+	@media only screen and (min-width: 720px) {
+		position: sticky;
+		margin: 0;
+		padding: 0.5em 64px;
+		border-top: 1px solid #b8d2cf;
+		grid-column: 1 / span 2;
+		top: 64px;
+	}
 
-  @media only screen and (min-width: 1000px) {
-    display: none;
-  }
+	@media only screen and (min-width: 1000px) {
+		display: none;
+	}
 }
 
 #programs {
-  list-style: none;
-  display: grid;
-  grid-template-rows: var(--list);
+	list-style: none;
+	display: grid;
+	grid-template-rows: var(--list);
 
-  li {
-    a {
-      display: block;
-      height: 100%;
-      contain: layout;
-      color: inherit;
-    }
+	li {
+		a {
+			display: block;
+			height: 100%;
+			contain: layout;
+			color: inherit;
+		}
 
-    @media only screen and (min-width: 1000px) {
-      position: relative;
+		@media only screen and (min-width: 1000px) {
+			position: relative;
 
-      article {
-        position: sticky;
-        position: -webkit-sticky;
-        top: 130px;
-      }
-    }
-  }
+			article {
+				position: sticky;
+				position: -webkit-sticky;
+				top: 130px;
+			}
+		}
+	}
 
-  @media only screen and (max-width: 719px) {
-    margin-top: -56px;
-  }
+	@media only screen and (max-width: 719px) {
+		margin-top: -56px;
+	}
 
-  @media only screen and (min-width: 720px) {
-    grid-template-columns: 120px auto;
-    grid-column: 2;
-  }
+	@media only screen and (min-width: 720px) {
+		grid-template-columns: 120px auto;
+		grid-column: 2;
+	}
 
-  @media only screen and (min-width: 1000px) {
-    grid-template-rows: var(--table);
-    grid-template-areas: 'roomIB101 roomIB201 roomIB202 roomIB301 roomIB302 roomIB304 roomIB305 roomIB306 roomIB307 roomIB308 roomIB401 roomIB501 roomIB502 roomIB503 room-5927-7528-5DE5-574A';
-    grid-column: var(--room);
-    grid-gap: 0 0.5em;
-    grid-template-columns: repeat(var(--length), calc(((100% - 7 * 0.5em) / 8)));
-    margin: 0;
-  }
+	@media only screen and (min-width: 1000px) {
+		grid-template-rows: var(--table);
+		grid-template-areas: var(--areas);
+		grid-column: var(--room);
+		grid-gap: 0 0.5em;
+		grid-template-columns: repeat(var(--length), calc(((100% - 7 * 0.5em) / 8)));
+		margin: 0;
+	}
 }
 
 .program {
-  padding: 0.5em;
+	padding: 0.5em;
 
-  h4 {
-    font-weight: normal;
-    margin: 0;
-    word-break: break-word;
-    font-size: 16px;
-  }
+	h4 {
+		font-weight: normal;
+		margin: 0;
+		word-break: break-word;
+		font-size: 16px;
+	}
 
-  .track {
-    color: #009a79;
-  }
+	.track {
+		color: #009a79;
+	}
 
-  footer {
-    position: relative;
-  }
+	footer {
+		position: relative;
+	}
 
-  .period {
-    opacity: 0.8;
-    display: none;
-  }
+	.period {
+		opacity: 0.8;
+		display: none;
+	}
 
-  .length {
-    &::before, &::after {
-      content: ' - ';
-    }
-  }
+	.length {
+		&::before, &::after {
+			content: ' - ';
+		}
+	}
 
-  @media only screen and (max-width: 719px) {
-    border-bottom: 1px solid #e9e9e9;
-    padding: 1em;
-  }
+	@media only screen and (max-width: 719px) {
+		border-bottom: 1px solid #e9e9e9;
+		padding: 1em;
+	}
 
-  @media only screen and (min-width: 720px) {
-    grid-column: 2;
-    margin-bottom: 1em;
+	@media only screen and (min-width: 720px) {
+		grid-column: 2;
+		margin-bottom: 1em;
 
-    h4 {
-      font-size: 24px;
-    }
-  }
+		h4 {
+			font-size: 24px;
+		}
+	}
 
-  @media only screen and (min-width: 1000px) {
-    grid-column: var(--room);
-    grid-row-start: var(--start);
-    grid-row-end: var(--end);
-    border: 1px dashed rgba(0, 0, 0, 0.4);
-    border-radius: 5px;
-    margin-bottom: 0;
+	@media only screen and (min-width: 1000px) {
+		grid-column: var(--room);
+		grid-row-start: var(--start);
+		grid-row-end: var(--end);
+		border: 1px dashed rgba(0, 0, 0, 0.4);
+		border-radius: 5px;
+		margin-bottom: 0;
 
-    h4 {
-      font-size: large;
-    }
+		h4 {
+			font-size: large;
+		}
 
-    .period {
-      display: block;
-    }
+		.period {
+			display: block;
+		}
 
-    .track {
-      font-size: smaller;
-    }
+		.track {
+			font-size: smaller;
+		}
 
-    .length, .location, .language {
-      display: none;
-    }
-  }
+		.length, .room, .language {
+			display: none;
+		}
+	}
 }
 </style>
