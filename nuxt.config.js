8d101909e3f3aebf8f2acc03e77a3e93be02e222
Erickson Zhuang <bingluen0224@gmail.com>
2019-06-07 14:01:19 +0800

    Fix meta (og:description)

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-      { hid: 'og:description', name: 'og:description', content: 'COSCUP 2019' },
+      { hid: 'og:description', property: 'og:description', content: 'COSCUP 2019' },
       { hid: 'og:title', property: 'og:title', content: 'COSCUP 2019' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'COSCUP 2019' },
